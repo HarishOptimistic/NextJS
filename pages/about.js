e93a355b85207b7e77bcc58b30b8c9823b316419
Harish Radhakrishnan <harish@haplabz.com>
2020-06-23 10:20:21 +0530

    adding pages, components and style jsx

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Navbar from '../components/navbar';
+
+const About = () => (
+ <div>
+  <Navbar/>
+  <h1>About</h1>
+  <p>Coding to learn Next.js</p>
+ </div>
+);
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Navbar from '../components/navbar';
+
+const Index = () => (
+ <div>
+  <Navbar />
+  <h1>Welcome to learning Next.js</h1>
+ </div>
+);
+
+export default Index;
