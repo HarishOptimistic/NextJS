6e6c3299fb4aab428a86775f3099d9d618683414
Harish Radhakrishnan <harish@haplabz.com>
2020-06-23 10:38:23 +0530

    added bootswatch and removed style jsx

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bcbd7639ceca5fbe9748";
+/******/ 	var hotCurrentHash = "5736fa295a19991c8c81";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/Navbar.js
+++ components/Navbar.js
@@ -7,26 +7,6 @@ const Navbar = () => (
   <li><Link href="/"><a>Home</a></Link></li>
   <li><Link href="/about"><a>About</a></Link></li>
  </ul>
-
- {/* Style jsx */}
-<style jsx>{`
-ul {
- background: #333;
- color: #fff;
- list-style: none;
- display: flex;
-}
-
-ul li {
- font-size: 18px;
- margin-right: 20px;
-}
-
-ul li a {
- color: #fff;
- text-decoration: none;
-}
-`}</style>
  </div>
 );
 
