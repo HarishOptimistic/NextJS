5e470f4d6e0a00e93bfd036901bebf927b7f38e6
Harish Radhakrishnan <harish@haplabz.com>
2020-06-29 12:09:09 +0530

    fixed syntax warnings

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0c060673a900efa5ad7a";
+/******/ 	var hotCurrentHash = "80edfda581d63c8d8b76";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
